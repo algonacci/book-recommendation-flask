b703fbe137311f9edf728748f56e92af92178dad
algonacci <algonacci1@gmail.com>
2023-05-10 15:56:07 +0700

    formatting

--- app.py
+++ app.py
@@ -28,7 +28,6 @@ def title():
         return render_template("index.html")
 
 
-
 @app.route("/keyword", methods=["GET", "POST"])
 def keyword():
     if request.method == "POST":
@@ -38,24 +37,26 @@ def keyword():
     else:
         return render_template("index.html")
 
+
 @app.errorhandler(500)
 def internal_server_error(error):
     return render_template("recommendation.html", error="Judul novel yang dicari tidak ada"), 500
+
 
 @app.route("/daftar_novel")
 def daftar_novel():
     df = pd.read_csv('Novel.csv')
 
     # Add image column to DataFrame
-    df['Sampul'] = df['Sampul'].apply(lambda x: f'<img src="{x}" width="100">' if isinstance(x, str) else '')
+    df['Sampul'] = df['Sampul'].apply(
+        lambda x: f'<img src="{x}" width="100">' if isinstance(x, str) else '')
 
     # Convert the DataFrame to an HTML table with Bootstrap classes
-    table = df.to_html(index=False, classes='table table-striped', escape=False)
+    table = df.to_html(
+        index=False, classes='table table-striped', escape=False)
 
     # Render the HTML template with the Bootstrap table
     return render_template('daftar_novel.html', table=table)
-
-
 
 
 if __name__ == "__main__":
