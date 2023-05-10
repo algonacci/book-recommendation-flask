7f5e081775a07a61307cbe4ee968103c55ca3d56
algonacci <algonacci1@gmail.com>
2023-05-10 15:12:06 +0700

    updates

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import pandas as pd
 import module as md
 
 app = Flask(__name__)
@@ -18,10 +19,14 @@ def recommendation():
 def title():
     if request.method == "POST":
         judul = request.form["judul"]
-        recommendation = md.rec_pvdbow(Title=judul)
-        return render_template("title.html", recommendation=recommendation, judul=judul)
+        try:
+            recommendation = md.rec_pvdbow(Title=judul)
+            return render_template("title.html", recommendation=recommendation, judul=judul)
+        except KeyError:
+            return render_template("recommendation.html", error="Judul novel yang dicari tidak ada"), 500
     else:
         return render_template("index.html")
+
 
 
 @app.route("/keyword", methods=["GET", "POST"])
@@ -32,6 +37,25 @@ def keyword():
         return render_template("keyword.html", recommendation=recommendation, keyword=keyword)
     else:
         return render_template("index.html")
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template("recommendation.html", error="Judul novel yang dicari tidak ada"), 500
+
+@app.route("/daftar_novel")
+def daftar_novel():
+    df = pd.read_csv('Novel.csv')
+
+    # Add image column to DataFrame
+    df['Sampul'] = df['Sampul'].apply(lambda x: f'<img src="{x}" width="100">' if isinstance(x, str) else '')
+
+    # Convert the DataFrame to an HTML table with Bootstrap classes
+    table = df.to_html(index=False, classes='table table-striped', escape=False)
+
+    # Render the HTML template with the Bootstrap table
+    return render_template('daftar_novel.html', table=table)
+
+
 
 
 if __name__ == "__main__":
